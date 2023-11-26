8be9a7a2e779afc1bbc5d885553e696a7822fc33
mhd-eymenkaram <mohamadkaram839@gmail.com>
2023-11-26 20:33:13 +0300

    router link moving

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,6 +4,7 @@ import { ProductsDetialsComponent } from './components/products-detials/products
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductComponent } from './components/product/product.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -15,7 +16,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   exports:[
     ProductComponent
