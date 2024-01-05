faca852c48093977f131d22c19e0751f1c7f9a3b
mhd-eymenkaram <mohamadkaram839@gmail.com>
2024-01-06 02:34:27 +0300

    Cart Products Actions

--- src/app/carts/components/carts/carts.component.ts
+++ src/app/carts/components/carts/carts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { CartsService } from '../../sevices/carts.service';
 
 @Component({
   selector: 'app-carts',
@@ -6,10 +7,12 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./carts.component.scss']
 })
 export class CartsComponent implements OnInit {
+
   cartProducts: any[] = [];
   priceTotal:any;
-
-  constructor() {}
+  index:any;
+  success:boolean=false;
+  constructor(private service:CartsService) {}
   
   ngOnInit(): void {
     this.getCartProducts();
@@ -29,6 +32,64 @@ export class CartsComponent implements OnInit {
       total += product.item.price * product.quantity; 
     }
     this.priceTotal = total;
-  }
-  
+  };
+
+  minsAmount(index:number) {
+
+    if (this.cartProducts[index].quantity > 1) {
+      this.cartProducts[index].quantity--
+    this.getCartsTotal();
+    localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+    }
+    
+  };
+  addAmount(index:number) {
+    this.cartProducts[index].quantity++
+    this.getCartsTotal();
+    localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+
+    }
+
+    deleteProduct(index:number){
+      this.cartProducts.splice(index,1)
+      localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+      this.getCartsTotal();
+    }
+
+    clearCart(){
+      this.cartProducts=[]
+      localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+      this.getCartsTotal();
+    }
+
+    detectChange(){
+      localStorage.setItem("cart", JSON.stringify(this.cartProducts))
+      this.getCartsTotal();
+    }
+
+    addCart(){
+      let products =this.cartProducts.map(item=>{
+        return{
+          productId:item.item.id,
+          quantity:item.quantity
+        }
+      })
+      let Model = {
+        userId:5,
+        date: new Date(),
+        products: products
+      }
+      
+      if(this.cartProducts.length !== 0) {
+        this.service.creatNewCart(Model).subscribe(res => {
+          this.success = true;
+          console.log(Model);
+        });
+      }
+      
+       
+
+
+    }
+
 }
--- src/app/carts/sevices/carts.service.ts
+++ src/app/carts/sevices/carts.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EnvironmentInjector, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  creatNewCart(model:any){
+    return this.http.post("https://fakestoreapi.com/carts", model);
+
+  }
 }
